84288bba0d099936259f16478e95c1aefe6a3e63
Cillié Malan <cilliemalan@gmail.com>
2016-01-14 09:43:31 +0200

    Added Cargo.EntityFramework

---
+++ Cargo.EntityFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cargo.EntityFramework")]
+[assembly: AssemblyDescription("Entity Framework based data source for Cargo")]
+[assembly: AssemblyProduct("Cargo")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("ea3518c2-6d8e-4502-969f-0d385edddcf9")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
