53364a335f4615b5f74081055e5cb2c2a0ff74fd
Cillié Malan <cilliemalan@gmail.com>
2016-01-22 12:20:08 +0200

    Added documentation for ContentContext

--- Cargo/ContentContext.cs
+++ Cargo/ContentContext.cs
@@ -11,10 +11,19 @@ namespace Cargo
     /// </summary>
     public class ContentContext : IContentContext
     {
+        /// <summary>
+        /// When <c>true</c>, editing is enabled and tokenized content tags will be rendered.
+        /// </summary>
         public virtual bool EditingEnabled { get; set; }
 
+        /// <summary>
+        /// The locale for the content. Not used at current.
+        /// </summary>
         public virtual string Locale { get; set; }
 
+        /// <summary>
+        /// The location for the content.
+        /// </summary>
         public virtual string Location { get; set; }
     }
 }
