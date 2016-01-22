a03425a0a7cc71054c9f52660d761c6b9f52ccc2
Cillié Malan <cilliemalan@gmail.com>
2016-01-22 12:17:27 +0200

    Added documentation for CargoExtensions

--- Cargo/CargoExtensions.cs
+++ Cargo/CargoExtensions.cs
@@ -12,11 +12,21 @@ namespace Cargo
     /// </summary>
     public static class CargoExtensions
     {
+        /// <summary>
+        /// Register the cargo API.
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder"/> provided by OWIN.</param>
+        /// <param name="cargoEngine">The <see cref="CargoEngine"/> to register the API for.</param>
         public static IAppBuilder UseCargo(this IAppBuilder app, CargoEngine cargoEngine)
         {
             return app.Use(typeof(CargoPipeline), cargoEngine);
         }
 
+        /// <summary>
+        /// Register the cargo API for deferred loading.
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder"/> provided by OWIN.</param>
+        /// <param name="deferredCargoEngineProc">A function that will provide a <see cref="CargoEngine"/> when requested, or <c>null</c> if it is not available yet.</param>
         public static IAppBuilder UseCargo(this IAppBuilder app, Func<CargoEngine> deferredCargoEngineProc)
         {
             return app.Use(typeof(CargoPipeline), deferredCargoEngineProc);
